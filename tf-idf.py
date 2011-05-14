b62e26080ec8c41479dc42ae4e8d7eaad537df08
Yan Lhert <info@yanlhert.com>
2011-05-14 03:05:15 -0700

    Liftoff on parsing rss feeds

--- tf-idf.py
+++ tf-idf.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python
 
 # Libraries needed to do cool stuffs.
-import urllib
-import htmlparser
+#import urllib
+import feedparser
 
-# Get a page
-page1 = "http://www.reddit.com/r/programming"
-socket = urllib.urlopen(page1)
-r_programming_html = socket.read()
-socket.close()
-
-
-# Get a page
-#page2 = "http://www.reddit.com/r/netsec"
-#socket = urllib.urlopen(page2)
-#page2 = socket.read()
-#socket.close()        
+# Get a feed (deprecated when moving to feedparser method)
+#page1 = "http://www.reddit.com/r/programming.rss"
+#socket = urllib.urlopen(page1)
+#page1_html = socket.read()
+#socket.close()
 
 
-# Parse the HTML
-page1_parser = htmlparser()
+              
+page1 = feedparser.parse("http://www.reddit.com/r/programming.rss")
+#print page1['entries'][0]['title'] <- Link title, what we want
+for link in page1['entries']:
+	print link['title']
 
-page1_parser.feed(html)
+page2 = feedparser.parse("http://www.reddit.com/r/netsec.rss")
+for link in page2['entries']:
+	print link['title']    
