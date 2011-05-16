60e354f65d129c9774d5c3816d53c291d123f934
Yan Lhert <info@yanlhert.com>
2011-05-16 14:02:28 -0700

    making progress on parsing urls

--- tf-idf.py
+++ tf-idf.py
@@ -1,22 +1,35 @@
 #!/usr/bin/python
 
 # Libraries needed to do cool stuffs.
-#import urllib
 import feedparser
 
-# Get a feed (deprecated when moving to feedparser method)
-#page1 = "http://www.reddit.com/r/programming.rss"
-#socket = urllib.urlopen(page1)
-#page1_html = socket.read()
-#socket.close()
+class tfidf(object):
+	links1 = []
+	links2 = []
+	
+	def __init__(self, url1, url2):
+		self.url1 = url1
+		self.url2 = url2
+		
+		self.parsed1 = feedparser.parse(self.url1)
+		self.parsed2 = feedparser.parse(self.url1)
+
+		for x in range(1, 25):
+			self.links1.append(self.parsed1.entries[x].title)
+   			self.links2.append(self.parsed2.entries[x].title)    
+
+    def linkcompare(self):
+        print self.links
+
+		
+
+	
+proggit = tfidf("http://www.reddit.com/r/programming.rss", "http://www.reddit.com/r/netsec.rss")
+print proggit.links1
+print proggit.links2
+proggit.linkcompare()
 
 
-              
-page1 = feedparser.parse("http://www.reddit.com/r/programming.rss")
-#print page1['entries'][0]['title'] <- Link title, what we want
-for link in page1['entries']:
-	print link['title']
-
-page2 = feedparser.parse("http://www.reddit.com/r/netsec.rss")
-for link in page2['entries']:
-	print link['title']    
+#page2 = feedparser.parse("http://www.reddit.com/r/netsec.rss")
+#for link in page2['entries']:
+#	print link['title']    
