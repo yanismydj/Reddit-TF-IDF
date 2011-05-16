0563f4facedfd8cb8d6e5df8e41c2dbe5f4345e4
Yan Lhert <info@yanlhert.com>
2011-05-16 16:21:55 -0700

    counting up the number of times each term appears

--- tf-idf.py
+++ tf-idf.py
@@ -3,33 +3,56 @@
 # Libraries needed to do cool stuffs.
 import feedparser
 import math
+from operator import itemgetter
 
-class tfidf(object):
+
+class TFIDF(object):
 	links = []
 	terms = []
+	term_freq = {}
+	total_terms = 0
 	
 	def __init__(self, url):
 		self.url = url
 		
+		# Use feedparser to parse the content from the reddit rss feed
 		self.parsed = feedparser.parse(self.url)
 		
 		# Go through the first 25 Links on given reddit page
 		for x in range(1, 25):
 			self.links.append(self.parsed.entries[x].title)
+		
+		# We populate the term frequency for the links of this subreddit here
+		self.term_count()
 	
+	# Take our links and break them up into individual terms
 	def convert_links_to_terms(self):
 		for link in self.links:
-			self.terms.append(link.split(' '))
+			for term in link.split(' '):
+				self.terms.append(term)
 	
+	# This method will count up the number of times a term appears and populate the term_freq dict
 	def term_count(self):
-		self.tf = count(terms)
+		self.convert_links_to_terms()
 		
+		# Iterate through the terms we've collected for this subreddit, if their frequency is more
+		# than twice, add them to the term frequency list, otherwise ignore them,
+		for term in self.terms:
+		#	if self.terms.count(term) > 1:
+			self.term_freq[term] = self.terms.count(term)
+			self.total_terms = self.total_terms + self.terms.count(term)
+			
+	# calculate the term weight
+	def calc_tf(self):
+		print 'wewt'
 
-	
-proggit = tfidf("http://www.reddit.com/r/programming.rss")
-print proggit.links
-proggit.convert_links_to_terms()
-print proggit.terms
+
+
+
+proggit = TFIDF("http://www.reddit.com/r/programming.rss")
+print proggit.term_freq
+#for term in proggit.term_freq
+#	print term
 
 
 #page2 = feedparser.parse("http://www.reddit.com/r/netsec.rss")
